fc44da2a8d69e5be89d968ad784e7702b4d04257
aleksandarLazic1998 <aleksandar.lazic.p@gmail.com>
2023-09-22 10:45:14 +0200

    Initial

---
+++ SinglyLinkedList/SinglyLinkedList.js
@@ -0,0 +1,8 @@
+/*
+Singly Linked List
+
+Linearna data struktura koja nema index.
+Svaki element linked liste je node.
+Node moze bili koji data type i sadrzi pointer ka sledecem node-u.
+Kraj linked liste naziva se tail. Tail = null
+*/
